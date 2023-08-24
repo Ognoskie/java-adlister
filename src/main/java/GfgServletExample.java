75ee3b3d4490f0fde46732a2111ff17a7a35577e
Braden Wittkop <bradenjwittkop@gmail.com>
2023-08-24 18:45:41 -0500

    finished servlet exercise

---
+++ src/main/java/GfgServletExample.java
@@ -0,0 +1,43 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/count")
+public class GfgServletExample extends HttpServlet {
+	private int hitCount;
+
+	public void init() {
+		// Reset hit counter.
+		hitCount = 0;
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request,
+					  HttpServletResponse response)
+			throws ServletException, IOException {
+		// Set response content type
+		response.setContentType("text/html");
+
+		// This method executes whenever the servlet is hit
+		// increment hitCount
+		hitCount++;
+		PrintWriter out = response.getWriter();
+		String title = "Count of total number of hits";
+		String docType
+				= "<!doctype html public \"-//w3c//dtd html 4.0 "
+				+ "transitional//en\">\n";
+
+		out.println(
+				docType + "<html>\n"
+						+ "<head><title>" + title + "</title></head>\n"
+						+ "<body bgcolor = \"#f0f0f0\">\n"
+						+ "<h1 align = \"center\">" + title + "</h1>\n"
+						+ "<h2 align = \"center\">" + hitCount
+						+ "</h2>\n"
+						+ "</body>"
+						+ "</html>");
+	}
+}
